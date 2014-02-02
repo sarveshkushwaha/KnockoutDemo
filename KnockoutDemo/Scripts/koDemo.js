3411a9aa399711cc938642f89e6d477999838624
sarveshkushwaha <sarveshkumar@hotmail.co.in>
2014-02-02 07:41:28 +0530

    Added Observable Arrays

--- KnockoutDemo/Scripts/koDemo.js
+++ KnockoutDemo/Scripts/koDemo.js
@@ -7,8 +7,27 @@ $(function () {
 
     var viewModel = { name : ko.observable("I hate Hello World :)") };//intialize model property
     viewModel.name("change the value"); //set the value
-    alert(viewModel.name()); // Get the value
-    ko.applyBindings(viewModel); //Make it Two way Thing :)
+    //alert(viewModel.name()); // Get the value
+    ko.applyBindings(viewModel, document.getElementById('Observable')); //Make it Two way Thing :)
+
+    //#endregion
+    
+    //#region  Observable Arrays
+    var data = [{ name: "sarvesh" }, { name: "Akshay" }, { name: "isha" }, { name: "divya" }, { name: "priya" }];
+    
+    var ObservableArray = {
+            // data
+            items: ko.observableArray(data),
+            itemToAdd: ko.observable(""),
+
+            // behaviors
+            addItem: function () {
+                this.items.push({ name: this.itemToAdd() });
+                this.itemToAdd("");
+            }
+        };
+    
+    ko.applyBindings(ObservableArray, document.getElementById('ObservableArrayDiv'));
 
     //#endregion
 
